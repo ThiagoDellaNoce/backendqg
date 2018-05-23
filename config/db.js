11d7c659ea071181c5eaa2fd257b1a8add25bc9f
Thiago Della Noce <thiagodellanoce22@gmail.com>
2018-05-23 15:38:52 -0300

    Correção no banco

--- config/db.js
+++ config/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-    DB: 'mongodb://localhost:27017/angularcrud'
+    DB: 'mongodb://heroku_6cnpww8w:cuj7rthrqga4ojqfebo6ond2oa@ds133570.mlab.com:33570/heroku_6cnpww8w/angularcrud'
  };
